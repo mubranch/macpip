3df93905c457f30e62e8c8396d1cd0d18b7473ef
Mustafa Branch <mustafa.branch@gmail.com>
2023-03-26 15:14:37 -0600

    update

--- setup.py
+++ setup.py
@@ -6,22 +6,22 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read()
 
 setup(
-    name="mpip",
+    name="macpip",
     version="0.0.1",
     author="mbranch",
     author_email="",
     license="MIT",
     description="A tool to output macOS apps in requirements format.",
     url="",
-    py_modules=["mpip, 'app"],
+    py_modules=["macpip, 'app"],
     packages=find_namespace_packages(),
     install_requires=[requirements],
     python_requires=">=3.6",
     classifiers=[
         "Programming Language :: Python :: 3.11",
-        "Operating System :: MACOS",
+        "Operating System :: OS Independent",
     ],
     entry_points="""
     [console_scripts]]
-    mpip = mpip:cli""",
+    macpip = macpip:cli""",
 )
